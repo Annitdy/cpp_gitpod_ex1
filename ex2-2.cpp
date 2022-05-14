bce3130f9dc850b7a329aa2b9de1010188ee31ee
Annitdy <104436851+Annitdy@users.noreply.github.com>
2022-05-15 01:39:37 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,x,a;
+    printf( " please input a integer \n" );
+    scanf("%d",&i);
+    printf( " please input a integer \n" );
+    scanf("%d",&x);
+    printf( " your input integer= %d\n",a=x*x+i*i);
+    
+    
+    return 0;
+}
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    double f;
+    float f1;
+    printf( " please input a float number\n" );
+    scanf("%lf",&f);
+    printf( " your input float number= %lf\n",f=f*f*3.14 );
+    scanf("%lf",&f);
+    printf( " your input float number= %lf\n",f=f*2*3.14 );
+    
+    
+    
+    
+    return 0;
+}
